109c9709702657b8178fcdc0db97c33b89460853
Cem <cemkgrbz@gmail.com>
2022-08-23 13:57:18 +0200

    typo

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ console.log(squareNumber(5));
 
 //2
 
-function sqaure(arr, cb){
+function square(arr, cb){
     let newArr = []
     for(let i of arr){
         newArr.push(cb(i))
     } console.log(newArr);
 }
 const array = [1, 2, 3, 4, 5, 6]
-sqaure(array,squareNumber)  // return [1,4,9,16,25]
+square(array,squareNumber)  // return [1,4,9,16,25]
